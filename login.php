86c2a3268bd8ae42c6c5b8e73217d796e3a0a588
VIC_PMS <joao188ac@gmail.com>
2017-10-01 22:19:17 -0300

    Validação de login e página principal

--- login.php
+++ login.php
@@ -11,11 +11,11 @@
         if (mysqli_num_rows($verifica)<=0){
           echo"<script language='javascript' type='text/javascript'>alert('Login e/ou senha incorretos');window.location.href='index.html';</script>";
           die();
-        }else{
+        }else{  
           setcookie("login",$login);
-          header("Location:index.html");
+          header("Location:principal.html");
           
         }
-    }
+    }  
 ?>
 
